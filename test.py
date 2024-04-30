ec3242b9c3889ab40b82341ddc1820c408e77ac4
ddeellttaa <syzbw20@163.com>
2024-04-30 20:20:06 +0800

    test6:add the avg_loss and the code is right

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import StepLR
 from matplotlib import pyplot as plt
+import numpy as np
 '''
 python reforcement learning 
 '''
@@ -38,20 +39,21 @@ class DQNAgent(object):
         print(self.device)
         in_dim = self.env.observation_space.shape[0]
         out_dim = self.env.action_space.n
-        gamma = 0.9
+        gamma = 0.99
         epsilon = 0.25
         
         losses = []
-        rewards = []        
+        rewards = [] 
+        avg_rewards = []       
 
         model = DQN(in_dim, out_dim).to(self.device)
        # model =  torch.load("data/model1999.pth", map_location = self.device)
         model.train()
         loss = nn.MSELoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.0004)
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
         scheduler = StepLR(optimizer, step_size=50, gamma=0.7)    
         state =  self.env.reset()
-        state = torch.FloatTensor(state).to(self.device)
+        state = torch.from_numpy(state).float().to(self.device)
 
         epoch = 2000
         epochs = [] 
@@ -63,6 +65,7 @@ class DQNAgent(object):
             J=0
             C=0
             l = 0
+
             model.train()
             while done != 1:
                 C+=1
@@ -74,10 +77,9 @@ class DQNAgent(object):
                 action = int(action)
                 next_state, reward, done, _ = self.env.step(action)
                 R+=reward
-                next_state = torch.FloatTensor(next_state).to(self.device)
+                next_state = torch.from_numpy(next_state).float().to(self.device)
                 next_acton = torch.argmax(model(next_state))
                 #reward = torch.tensor([reward], device=self.device)
-                
                 q_value = model(state)[action]
                 e_q_value = reward + gamma * model(next_state)[next_acton].detach()*(1 - done)
               
@@ -95,8 +97,9 @@ class DQNAgent(object):
                 #     self.env.render()
             losses.append(J/C)
             rewards.append(R)
+            avg_rewards.append(np.mean(rewards[-100:]))
             epochs.append(i)
-            print(f"epoch:{i}  rewards:{R}")
+            print(f"epoch:{i}  rewards:{R} avg_reward:{np.mean(rewards[-100:])} time:{C}")
       
             if (i+1) % 200 == 0:
                 torch.save(model, f'model{i}.pth')
@@ -105,14 +108,15 @@ class DQNAgent(object):
             self.env.close()
             scheduler.step()
             if (i + 1) % 100 ==0:
-                plt.figure(figsize=(12,6))
+                plt.figure(figsize=(24,6))
                 plt.plot(epochs,losses,label="loss")
                 plt.xlabel("epoch")
                 plt.legend()
                 plt.savefig(f"model{i}loss.png", dpi = 400)
                 plt.close() 
-                plt.figure(figsize=(12,6))
+                plt.figure(figsize=(24,6))
                 plt.plot(epochs,rewards,label="reward")
+                plt.plot(epochs,avg_rewards,label = "avg_reward")
                 plt.xlabel("epoch")
                 plt.legend()
                 plt.savefig(f"model{i}reward.png", dpi = 400)
